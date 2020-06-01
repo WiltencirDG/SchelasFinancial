1d9b9b1a24e6f788dad7c00f54c49f41b509fb7b
Wiltencir <wiltencir@gmail.com>
2020-06-01 14:10:46 -0300

    Server

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-var express = require("express");
-const state = require('./robots/state.js')
-const http = require('http')
-const port = process.env.PORT
 const robots = {
-    spreadsheet: require('./robots/spreadsheet.js')
+    spreadsheet: require('./robots/spreadsheet.js'),
+    server: require('./robots/server.js')
 }
-
-const app = express();
 
 async function start(){
     
-    const server = await createServer()
     await robots.spreadsheet()
-    const content = state.load()
-    await apiCalls()
+    await robots.server()
 
-    //console.dir(content, {depth: null})
-    
-    async function createServer(){
-        return new Promise((resolve, reject) => {
-            const server = app.listen(port, (error) => {
-                if(error){
-                    reject(error)
-                }
-                console.log(`App now running on: ${server.address()}:${port}`);
-                resolve(server)
-            });
-
-        })
-    }
-
-    async function apiCalls(){
-        app.get("/index.js", (req, res) => {
-            res.statusCode = 200
-            res.end(JSON.stringify(content))
-        });
-    }
 
 }
 
