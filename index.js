34270310aaf6f7517c68abdb38286ed00e76954c
Wiltencir <wiltencir@gmail.com>
2020-06-01 13:56:36 -0300

    Mounting api calls

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
+var express = require("express");
 const state = require('./robots/state.js')
 const http = require('http')
-const port = 443
+const port = process.env.PORT
 const robots = {
     spreadsheet: require('./robots/spreadsheet.js')
 }
 
+const app = express();
+
 async function start(){
     
+    const server = await createServer()
     await robots.spreadsheet()
-
     const content = state.load()
+    await apiCalls()
 
     //console.dir(content, {depth: null})
-
-    const server = http.createServer((request, response) => {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'application/json')
-        response.end(content)
-    })
-
-    server.listen(port, () => {
-        console.log('Server started at: https://schelas-financial.herokuapp.com/index.js')
-    })
     
+    async function createServer(){
+        return new Promise((resolve, reject) => {
+            const server = app.listen(port, (error) => {
+                if(error){
+                    reject(error)
+                }
+                console.log(`App now running on: ${server.address()}:${port}`);
+                resolve(server)
+            });
+
+        })
+    }
+
+    async function apiCalls(){
+        app.get("/index.js", (req, res) => {
+            res.statusCode = 200
+            res.end(content)
+        });
+    }
 
 }
 
