c55781c9f648f699a43f66a96a8afde62b625686
Wiltencir <wiltencir@gmail.com>
2020-06-01 12:45:15 -0300

    Test response

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const fs = require('fs')
 const state = require('./robots/state.js')
-
-
+const http = require('http')
+const port = 443
 const robots = {
     spreadsheet: require('./robots/spreadsheet.js')
 }
 
 async function start(){
-
+    
     await robots.spreadsheet()
 
     const content = state.load()
 
-    console.dir(content, {depth: null})
+    //console.dir(content, {depth: null})
+
+    const server = http.createServer((request, response) => {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'application/json')
+        response.end(content)
+    })
+
+    server.listen(port, () => {
+        console.log('Server started at: https://schelas-financial.herokuapp.com')
+    })
+    
 
 }
 
